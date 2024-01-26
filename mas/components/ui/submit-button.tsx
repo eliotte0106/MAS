3ecaf36d27a362e4932cbff1acfd5a7ebc29750c
Jihong <jihong2253@gmail.com>
2024-01-26 18:37:29 -0500

    add button

---
+++ mas/components/ui/submit-button.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Button } from './button'
+import { useFormStatus } from 'react-dom'
+import { Loader } from './loader'
+
+function SubmitButton({
+    title
+}: {
+    title: string
+}) {
+    const { pending } = useFormStatus()
+  return (
+    <Button
+    className="flex w-full justify-center rounded-md bg-black px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2"
+    type="submit"
+    >
+        {pending ? <Loader /> : title}
+    </Button>
+  )
+}
+
+export default SubmitButton
