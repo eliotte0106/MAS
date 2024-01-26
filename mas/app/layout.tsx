f973e51c9935c86b7161ed8eba7272c5e25fafc1
Jihong <jihong2253@gmail.com>
2024-01-26 18:34:43 -0500

    edit layout

--- mas/app/layout.tsx
+++ mas/app/layout.tsx
@@ -1,10 +1,7 @@
 import type { Metadata } from 'next'
-//import { Inter } from 'next/font/google'
 import './globals.css'
 import { Inter as FontSans } from "next/font/google"
 import { cn } from "@/lib/utils"
-
-//const inter = Inter({ subsets: ['latin'] })
 
 export const fontSans = FontSans({
   subsets: ["latin"],
