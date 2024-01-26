c85f826029a1b5d58c8d4649bd7076e992e8c354
Jihong <jihong2253@gmail.com>
2024-01-26 18:37:40 -0500

    modify password

--- mas/lib/utils.ts
+++ mas/lib/utils.ts
@@ -1,5 +1,6 @@
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
+import { hash, compare } from 'bcrypt-ts'
 
 const BASE_URL = "http://localhost:3000/api/"
 
@@ -10,4 +11,15 @@ export function cn(...inputs: ClassValue[]) {
 export function buildUrl(endPoint: string): string {
   const url = new URL(endPoint, BASE_URL)
   return url.href
+}
+
+export async function hashPasswrod(nakedPassword: string) {
+  const hashPasswrod = await hash(nakedPassword, 10)
+  return hashPasswrod
+}
+
+export async function comparePassword(nakedPassword: string, hashPassword: string) {
+  const result = await compare(nakedPassword, hashPassword)
+
+  return result
 }
